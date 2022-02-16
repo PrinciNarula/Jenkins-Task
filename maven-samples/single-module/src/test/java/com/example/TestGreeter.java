b7c4b004a78bbd69f1b67e094645bd1a7b741c8e
PrinciNarula <princinarula@gmail.com>
2022-02-16 12:03:19 +0530

    testfail

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -7,6 +7,7 @@ import org.mockito.internal.matchers.LessThan;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
@@ -37,6 +38,6 @@ public class TestGreeter {
   public void greetShouldIncludeGreet() {
     String someone = "Hello World, This is my first JUnit Trend";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(lessThan(someone.length())));
   }
 }
